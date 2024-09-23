81b6812e2bcebe1a1b098f53b4698853d0337262
abumusa8 <abumusa0740@gmail.com>
2024-09-23 13:10:03 +0600

    update donate add

--- js/index.js
+++ js/index.js
@@ -8,12 +8,24 @@ document.getElementById('donait-now-btn').addEventListener('click', function(eve
    const loss =mainblance-inputbtn
       document.getElementById('update-donate').innerText= newblance
       document.getElementById('main-blance').innerText =loss
-console.table({inputbtn, mainblance, updatedonate, newblance, loss})
-   
-
- 
-   
+    const modal = document.getElementById('my_modal_5');
+    modal.showModal(); 
    
 })
 
 
+document.getElementById('donait-now-btn2').addEventListener('click', function(even){
+    even.preventDefault()
+   const inputbtn = parseFloat(document.getElementById('input-btn').value)
+   const mainblance = parseFloat(document.getElementById('main-blance').innerText)
+   const updatedonate= parseFloat(document.getElementById('update-donate2').innerText)
+   console.log(inputbtn , mainblance , updatedonate)
+   
+   const newblance = inputbtn+updatedonate
+   const loss =mainblance-inputbtn
+      document.getElementById('update-donate').innerText= newblance
+      document.getElementById('main-blance').innerText =loss
+    const modal = document.getElementById('my_modal_5');
+    modal.showModal(); 
+   
+})
